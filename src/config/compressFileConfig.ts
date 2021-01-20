8ed44b5869963b7b3d1fd610658e38e749eae45c
AlbertDev33 <alber725@gmail.com>
2021-01-20 16:57:13 -0300

    Implementação da chamada ao provider do S3

--- src/config/compressFileConfig.ts
+++ src/config/compressFileConfig.ts
@@ -1,20 +1,26 @@
 import path from 'path';
 import sharp from 'sharp';
+import uploadConfig from './upload';
+import S3StorageProvider from '../providers/implementations/S3StorageProvider';
+
+const s3StorageProvider = new S3StorageProvider();
 
 export const fileSize = async (fileName: string) => {
   const tempFolder = path.resolve(__dirname, '..', '..', 'tmp');
-  const filePath = path.resolve(__dirname, '..', '..', 'tmp', `${fileName}`);
+  const filePath = path.resolve(tempFolder, `${fileName}`);
 
   const [fileHashName, extension] = path.basename(filePath).split('.');
 
-  const destination = `${tempFolder}/uploads`;
+  uploadConfig.driver === 's3' ? s3StorageProvider.saveFile(fileName) : 'Erro no upload';
 
   const sizes = [128, 48, 32, 24, 16];
 
   sizes.forEach(async size => {
     await sharp(filePath)
-    .clone()
-    .resize({ width: size })
-    .toFile(`${destination}/${fileHashName}-${size}.${extension}`)
-  })
+      .clone()
+      .resize({ width: size })
+      .toFile(`${tempFolder}/${fileHashName}-${size}.${extension}`)
+    uploadConfig.driver === 's3' ? await s3StorageProvider.saveFile(`${fileHashName}-${size}.${extension}`) : 'Não fez o upload';
+  });
+
 }
