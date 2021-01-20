72fe012dd2f5ea73037f005a78b2422e54405af2
AlbertDev33 <alber725@gmail.com>
2021-01-20 16:54:06 -0300

    Organização do arquivo com a utilização de um controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,14 @@
 import { Router, Request, Response } from 'express';
 import multer from 'multer';
+
+import UploadFileController from '../controller/UploadFileController';
+
 import uploadConfig from '../config/upload';
-import { fileSize } from '../config/compressFileConfig';
 
 const routes = Router();
 const upload = multer(uploadConfig.multer);
+const uploadFileController = new UploadFileController();
 
-routes.post('/files', upload.single('file'), async (request: Request, response: Response) => {
-  const fileName = request.file.filename;
-
-  await fileSize(fileName);
-
-  return response.json(request.file);
-});
+routes.post('/files', upload.single('file'), uploadFileController.create)
 
 export default routes;
