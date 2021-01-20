4aa2b8bbb4a736642d2262ba6d3a0ad7ad72112f
AlbertDev33 <alber725@gmail.com>
2021-01-20 09:08:09 -0300

    Implementação do middware multer e a função fileSize que redimensiona as imagens

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,15 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+import { fileSize } from '../config/compressFileConfig';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
-routes.get('/', (request, response) => {
-  response.json({ message: 'Hello Backend' });
+routes.post('/files', upload.single('file'), async (request: Request, response: Response) => {
+  await fileSize(request.file);
+
+  return response.json(request.file);
 });
 
 export default routes;
