f0979aad2df95c613091ba877c02e87b4c0cbb47
AlbertDev33 <alber725@gmail.com>
2021-01-21 22:18:56 -0300

    Refatoração do código. Realizando a inversão de dependências e melhorando a legibilidade do código

--- src/config/compressFileConfig.ts
+++ src/config/compressFileConfig.ts
@@ -1,16 +1,14 @@
 import path from 'path';
 import sharp from 'sharp';
 import uploadConfig from './upload';
-import S3StorageProvider from '../providers/implementations/S3StorageProvider';
-import DiskStorageProvider from '../providers/implementations/DiskStorageProvider';
+import IStorageProvider from '../providers/models/IStorageProvider';
 import IUrlUploadsRepository from '../database/repositories/IUrlUploadsRepository';
-
-const s3StorageProvider = new S3StorageProvider();
-const diskStorageProvider = new DiskStorageProvider();
 
 class FileSize {
   constructor(
     private urlUploadsRepository: IUrlUploadsRepository,
+
+    private storageProvider: IStorageProvider,
   ){}
 
   public async execute(fileName: string): Promise<string> {
@@ -22,10 +20,8 @@ class FileSize {
     const urlUploas =
       `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${fileName}`;
 
-    uploadConfig.driver === 's3' ?
-      s3StorageProvider.saveFile(fileName) &&
+      this.storageProvider.saveFile(fileName)
       this.urlUploadsRepository.create(urlUploas)
-      : diskStorageProvider;
 
     const sizes = [128, 48, 32, 24, 16];
 
@@ -37,11 +33,10 @@ class FileSize {
           .resize({ width: size })
           .toFile(`${tempFolder}/${newFile}`)
 
-      uploadConfig.driver === 's3' ?
-        await this.urlUploadsRepository
+      await this.urlUploadsRepository
           .create(`https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${newFile}`)
-         && await s3StorageProvider.saveFile(newFile)
-          : await diskStorageProvider.saveFile(newFile);
+
+      await this.storageProvider.saveFile(newFile)
     });
 
     return fileName;
