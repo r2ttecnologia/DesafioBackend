e781c5cb9f7f2f3d0ae7dbeb2a3eab0eb6d11751
AlbertDev33 <alber725@gmail.com>
2021-01-21 22:20:04 -0300

    Passando a chamada dos Providers para o Controller

--- src/controller/UploadFileController.ts
+++ src/controller/UploadFileController.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from 'express';
 
 import FileSize from '../config/compressFileConfig';
+import S3StorageProvider from '../providers/implementations/S3StorageProvider';
+import DiskStorageProvider from '../providers/implementations/DiskStorageProvider';
 import UrlUploadsRepository from '../database/mongodb/typeorm/repositories/UrlUploadsRepository';
+import uploadConfig from '../config/upload';
+
+
+const s3StorageProvider = new S3StorageProvider();
+const diskStorageProvider = new DiskStorageProvider();
 
 export default class UploadFileController {
   public async create(request: Request, response: Response): Promise<Response> {
     const fileName = request.file.filename;
 
     const urlUploadsRepository = new UrlUploadsRepository();
-    const fileSize = new FileSize(urlUploadsRepository);
+    const storageProvider =
+      uploadConfig.driver === 's3' ? s3StorageProvider : diskStorageProvider;
+
+    const fileSize = new FileSize(urlUploadsRepository, storageProvider);
 
     await fileSize.execute(fileName);
 
