89ab5426fdcf74878345339c75cb33b8b1b712ce
AlbertDev33 <alber725@gmail.com>
2021-01-21 21:38:57 -0300

    Inclusão da chamada à conexão ao banco de dados

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,12 @@ import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 app.listen(3333, () => {
