bf0613e96af8db61e6c3c60f66c3215832e6fb98
AlbertDev33 <alber725@gmail.com>
2021-01-21 21:38:14 -0300

    Instanciação do Repository, geração das urls dinâmicamente

--- src/config/compressFileConfig.ts
+++ src/config/compressFileConfig.ts
@@ -3,32 +3,49 @@ import sharp from 'sharp';
 import uploadConfig from './upload';
 import S3StorageProvider from '../providers/implementations/S3StorageProvider';
 import DiskStorageProvider from '../providers/implementations/DiskStorageProvider';
+import UrlUploadsRepository from '../database/mongodb/typeorm/repositories/UrlUploadsRepository';
 
 const s3StorageProvider = new S3StorageProvider();
 const diskStorageProvider = new DiskStorageProvider();
 
-export const fileSize = async (fileName: string) => {
-  const tempFolder = path.resolve(__dirname, '..', '..', 'tmp');
-  const filePath = path.resolve(tempFolder, `${fileName}`);
+class FileSize {
+  constructor(
+    private urlUploadsRepository: UrlUploadsRepository,
+  ){}
 
-  const [fileHashName, extension] = path.basename(filePath).split('.');
+  public async execute(fileName: string): Promise<string> {
+    const tempFolder = path.resolve(__dirname, '..', '..', 'tmp');
+    const filePath = path.resolve(tempFolder, `${fileName}`);
 
-  uploadConfig.driver === 's3' ?
-    s3StorageProvider.saveFile(fileName) :
-    diskStorageProvider;
+    const [fileHashName, extension] = path.basename(filePath).split('.');
 
-  const sizes = [128, 48, 32, 24, 16];
+    const urlUploas =
+      `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${fileName}`;
 
-  sizes.forEach(async size => {
-    const newFile = `${fileHashName}-${size}.${extension}`;
-
-    await sharp(filePath)
-      .clone()
-      .resize({ width: size })
-      .toFile(`${tempFolder}/${newFile}`)
     uploadConfig.driver === 's3' ?
-      await s3StorageProvider.saveFile(`${newFile}`) :
-      await diskStorageProvider.saveFile(`${newFile}`);
-  });
+      s3StorageProvider.saveFile(fileName) &&
+      this.urlUploadsRepository.create(urlUploas)
+      : diskStorageProvider;
 
+    const sizes = [128, 48, 32, 24, 16];
+
+    sizes.forEach(async size => {
+      const newFile = `${fileHashName}-${size}.${extension}`;
+
+        await sharp(filePath)
+          .clone()
+          .resize({ width: size })
+          .toFile(`${tempFolder}/${newFile}`)
+
+      uploadConfig.driver === 's3' ?
+        await this.urlUploadsRepository
+          .create(`https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${newFile}`)
+         && await s3StorageProvider.saveFile(newFile)
+          : await diskStorageProvider.saveFile(newFile);
+    });
+
+    return fileName;
+    }
 }
+
+export default FileSize;
